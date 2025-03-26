175d9d306121887bd61614430f17ff14de81a408
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-26 13:50:36 +0100

    created class mainApp

---
+++ src/main/java/com/github/hanyaeger/tutorial/LastLineOfDefenseApp.java
@@ -0,0 +1,15 @@
+package com.github.hanyaeger.tutorial;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class LastLineOfDefenseApp extends YaegerGame {
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
