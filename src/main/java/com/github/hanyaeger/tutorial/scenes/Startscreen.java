d3fbd6b949a845d991cc1178cae95b084a906246
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-26 14:33:16 +0100

    merge with app

--- src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
@@ -1,6 +1,7 @@
 package com.github.hanyaeger.tutorial.scenes;
 
 import com.github.hanyaeger.api.scenes.StaticScene;
+import com.github.hanyaeger.tutorial.LastLineOfDefenseApp;
 
 public class Startscreen extends StaticScene {
     private LastLineOfDefenseApp app;
