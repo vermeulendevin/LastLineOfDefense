e4dd052cc52c7fe82a090a46df99527b7bc4b600
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-26 13:52:02 +0100

    setup Startscreen

--- src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
@@ -1,4 +1,15 @@
 package com.github.hanyaeger.tutorial.scenes;
 
-public class Startscreen {
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class Startscreen extends StaticScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
 }
