32b92aafd5da163ac427e770c9d6a368f03a99fe
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-26 14:01:03 +0100

    add comstructor

--- src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/Startscreen.java
@@ -3,6 +3,12 @@ package com.github.hanyaeger.tutorial.scenes;
 import com.github.hanyaeger.api.scenes.StaticScene;
 
 public class Startscreen extends StaticScene {
+    private LastLineOfDefenseApp app;
+
+    public Startscreen(LastLineOfDefenseApp app) {
+        this.app = app;
+    }
+
     @Override
     public void setupScene() {
 
