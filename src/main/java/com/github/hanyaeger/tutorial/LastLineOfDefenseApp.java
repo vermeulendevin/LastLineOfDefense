f39bc043206614b2177302f0ca2a8908c6142405
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-26 13:52:44 +0100

    add PSVM

--- src/main/java/com/github/hanyaeger/tutorial/LastLineOfDefenseApp.java
+++ src/main/java/com/github/hanyaeger/tutorial/LastLineOfDefenseApp.java
@@ -3,6 +3,10 @@ package com.github.hanyaeger.tutorial;
 import com.github.hanyaeger.api.YaegerGame;
 
 public class LastLineOfDefenseApp extends YaegerGame {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
     public void setupGame() {
 
