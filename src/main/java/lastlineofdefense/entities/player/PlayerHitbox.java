8e2a45e5fcc0f7506955bf8dc3a2adb4242e1e59
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-28 15:22:14 +0100

    updated classes

--- src/main/java/lastlineofdefense/entities/player/PlayerHitbox.java
+++ src/main/java/lastlineofdefense/entities/player/PlayerHitbox.java
@@ -1,4 +1,19 @@
 package lastlineofdefense.entities.player;
 
-public class PlayerHitbox {
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collided;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.impl.RectangleEntity;
+
+import java.util.List;
+
+public class PlayerHitbox extends RectangleEntity implements Collided {
+    protected PlayerHitbox(Coordinate2D initialLocation) {
+        super(initialLocation);
+    }
+
+    @Override
+    public void onCollision(List<Collider> list) {
+
+    }
 }
--- src/main/java/lastlineofdefense/entities/player/PlayerSprite.java
+++ src/main/java/lastlineofdefense/entities/player/PlayerSprite.java
@@ -1,4 +1,11 @@
 package lastlineofdefense.entities.player;
 
-public class PlayerSprite {
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.SpriteEntity;
+
+public class PlayerSprite extends SpriteEntity {
+
+    protected PlayerSprite(String resource, Coordinate2D initialLocation) {
+        super(resource, initialLocation);
+    }
 }
