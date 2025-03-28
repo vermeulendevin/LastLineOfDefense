8b58c205b13453b7ead3a6ad58538ac567f2660a
Marijn Zwart <amarijnzwart@gmail.com>
2025-03-28 16:10:47 +0100

    Added: soldier 1 sprite

--- src/main/java/lastlineofdefense/scenes/Pausescreen.java
+++ src/main/java/lastlineofdefense/scenes/Pausescreen.java
@@ -34,8 +34,8 @@ public class Pausescreen extends StaticScene {
         PauseText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
         addEntity(PauseText);
 
-        //TODO OPTIONAL SCORE/HIGHSCORE
-        //TODO RESUME BUTTON
+        //TODO: OPTIONAL SCORE/HIGHSCORE
+        //TODO: RESUME BUTTON
 
         var startOverButton = new PlayButton(app, new Coordinate2D(getWidth()/4, (getHeight()/4)*3), "Start Over");
         startOverButton.setAnchorPoint(AnchorPoint.CENTER_CENTER);
