58938f7ac837851e485c75b06a1c40ecd9da7305
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-28 16:00:27 +0100

    setup playerHitbox

--- src/main/java/lastlineofdefense/entities/player/PlayerHitbox.java
+++ src/main/java/lastlineofdefense/entities/player/PlayerHitbox.java
@@ -4,12 +4,16 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.impl.RectangleEntity;
+import javafx.scene.paint.Color;
 
 import java.util.List;
 
 public class PlayerHitbox extends RectangleEntity implements Collided {
     protected PlayerHitbox(Coordinate2D initialLocation) {
         super(initialLocation);
+        setWidth(100);
+        setHeight(100);
+        setFill(Color.TRANSPARENT);
     }
 
     @Override
