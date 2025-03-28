a0e842eab44a0d10317f1922ccca72d9894bff47
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-28 13:49:59 +0100

    start

---
+++ src/main/java/lastlineofdefense/LastLineOfDefenseApp.java
@@ -0,0 +1,4 @@
+package lastlineofdefense;
+
+public class LastLineOfDefenseApp {
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module waterworld {
+module lastLineOfDefense {
     requires hanyaeger;
 
     exports lastlineofdefense;
