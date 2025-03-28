ab450457e2c998974f9d881e16cbcd8a00e70e24
Marijn Zwart <amarijnzwart@gmail.com>
2025-03-28 15:42:55 +0100

    Added: all screens in setupScenes

--- src/main/java/lastlineofdefense/LastLineOfDefenseApp.java
+++ src/main/java/lastlineofdefense/LastLineOfDefenseApp.java
@@ -3,6 +3,7 @@ package lastlineofdefense;
 import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.YaegerGame;
 import lastlineofdefense.scenes.Endscreen;
+import lastlineofdefense.scenes.Gamescreen;
 import lastlineofdefense.scenes.Startscreen;
 
 public class LastLineOfDefenseApp extends YaegerGame {
@@ -19,6 +20,8 @@ public class LastLineOfDefenseApp extends YaegerGame {
     @Override
     public void setupScenes() {
         addScene(0, new Startscreen(this));
+        addScene(1, new Gamescreen(this));
+        addScene(2, new Pausescreen(this));
         addScene(3, new Endscreen(this));
     }
 }
