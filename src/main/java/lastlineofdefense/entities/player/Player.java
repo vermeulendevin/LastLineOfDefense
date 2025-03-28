81ea5918d0a62e432b492322d498e49e72847da0
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-28 15:51:09 +0100

    Added movement

--- src/main/java/lastlineofdefense/entities/player/Player.java
+++ src/main/java/lastlineofdefense/entities/player/Player.java
@@ -3,12 +3,22 @@ package lastlineofdefense.entities.player;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.DynamicCompositeEntity;
 import com.github.hanyaeger.api.entities.SceneBorderCrossingWatcher;
+import com.github.hanyaeger.api.entities.SceneBorderTouchingWatcher;
 import com.github.hanyaeger.api.scenes.SceneBorder;
+import com.github.hanyaeger.api.userinput.KeyListener;
+import javafx.scene.input.KeyCode;
+import lastlineofdefense.LastLineOfDefenseApp;
 
-public class Player extends DynamicCompositeEntity implements SceneBorderCrossingWatcher {
+import java.util.Set;
 
-    protected Player(Coordinate2D initialLocation) {
+public class Player extends DynamicCompositeEntity implements SceneBorderTouchingWatcher, KeyListener {
+
+    private LastLineOfDefenseApp app;
+    private byte health = 3;
+
+    protected Player(LastLineOfDefenseApp app, Coordinate2D initialLocation) {
         super(initialLocation);
+        this.app = app;
     }
 
     @Override
@@ -17,7 +27,16 @@ public class Player extends DynamicCompositeEntity implements SceneBorderCrossin
     }
 
     @Override
-    public void notifyBoundaryCrossing(SceneBorder sceneBorder) {
+    public void notifyBoundaryTouching(SceneBorder sceneBorder) {
 
+    }
+
+    @Override
+    public void onPressedKeysChange(Set<KeyCode> pressedKeys) {
+        if(pressedKeys.contains(KeyCode.A)){
+            setMotion(3,270d);
+        } else if(pressedKeys.contains(KeyCode.D)){
+            setMotion(3,90d);
+        }
     }
 }
