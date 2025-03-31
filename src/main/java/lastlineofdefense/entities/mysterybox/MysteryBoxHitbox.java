9e7e7b94d5e42447092a999a96ed6bc12b8f8609
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-03-31 11:00:09 +0200

    Added mysterybox hitbox

--- src/main/java/lastlineofdefense/entities/mysterybox/MysteryBoxHitbox.java
+++ src/main/java/lastlineofdefense/entities/mysterybox/MysteryBoxHitbox.java
@@ -1,4 +1,16 @@
 package lastlineofdefense.entities.mysterybox;
 
-public class MysteryBoxHitbox {
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.impl.RectangleEntity;
+import javafx.scene.paint.Color;
+
+public class MysteryBoxHitbox extends RectangleEntity implements Collider {
+
+    protected MysteryBoxHitbox(Coordinate2D initialLocation) {
+        super(initialLocation);
+        setWidth(100);
+        setHeight(100);
+        setFill(Color.TRANSPARENT);
+    }
 }
