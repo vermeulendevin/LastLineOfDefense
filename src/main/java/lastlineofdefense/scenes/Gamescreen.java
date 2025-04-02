22f647addf4a04ebcdb338227b88905c6e1dedf9
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-02 17:58:07 +0200

    Added: Bunkers to gamescreen

--- src/main/java/lastlineofdefense/scenes/Gamescreen.java
+++ src/main/java/lastlineofdefense/scenes/Gamescreen.java
@@ -4,6 +4,7 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import lastlineofdefense.LastLineOfDefenseApp;
 import lastlineofdefense.entities.bullet.Bullet;
+import lastlineofdefense.entities.bunker.Bunker;
 import lastlineofdefense.entities.player.Player;
 import lastlineofdefense.hud.scoreboard.Lives;
 import lastlineofdefense.hud.scoreboard.Scoreboard;
@@ -11,6 +12,8 @@ import lastlineofdefense.hud.scoreboard.Scoreboard;
 public class Gamescreen extends DynamicScene {
     private LastLineOfDefenseApp app;
     private Player player;
+
+    private int nrOfBunkers = 4;
 
     public Gamescreen(LastLineOfDefenseApp app) {
         this.app = app;
@@ -44,6 +47,10 @@ public class Gamescreen extends DynamicScene {
         for (int i = 0; i < numberOfLives; i++) {
             int livesX = LivesStartX + (i * LivesSpacing);
             addEntity(new Lives(new Coordinate2D(livesX, LivesY)));
+        }
+
+        for(int i = 0; i < nrOfBunkers; i++) {
+            addEntity(new Bunker(new Coordinate2D(100 + i * 300, getHeight() / 10 * 6.5)));
         }
     }
 
