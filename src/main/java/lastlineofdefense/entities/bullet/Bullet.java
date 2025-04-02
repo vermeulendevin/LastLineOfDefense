fdf16f77de9973da372966060bfc8653b2d8177e
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-02 14:19:48 +0200

    Renamed Bullet

--- src/main/java/lastlineofdefense/entities/bullet/Bullet.java
+++ src/main/java/lastlineofdefense/entities/bullet/Bullet.java
@@ -4,10 +4,10 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.impl.DynamicRectangleEntity;
 
-public class bullet extends DynamicRectangleEntity implements Collider {
+public class Bullet extends DynamicRectangleEntity implements Collider {
     private final double speed = 10;
 
-    public bullet(Coordinate2D initialLocation, double direction) {
+    public Bullet(Coordinate2D initialLocation, double direction) {
         super(initialLocation);
         setHeight(5);
         setWidth(2);
