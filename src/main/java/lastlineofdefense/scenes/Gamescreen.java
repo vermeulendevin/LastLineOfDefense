38e4ec99a2c5a76e481bdc8b743d83bc04cf97e4
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-02 14:03:43 +0200

    Added bunkers for loop

--- src/main/java/lastlineofdefense/scenes/Gamescreen.java
+++ src/main/java/lastlineofdefense/scenes/Gamescreen.java
@@ -3,10 +3,14 @@ package lastlineofdefense.scenes;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
 import lastlineofdefense.LastLineOfDefenseApp;
+import lastlineofdefense.entities.bunker.Bunker;
 import lastlineofdefense.entities.player.Player;
+
+import java.util.ArrayList;
 
 public class Gamescreen extends DynamicScene {
     private LastLineOfDefenseApp app;
+    private int nrBunkers = 4;
 
     public Gamescreen(LastLineOfDefenseApp app) {
         this.app = app;
@@ -20,6 +24,12 @@ public class Gamescreen extends DynamicScene {
 
     @Override
     public void setupEntities() {
-        addEntity(new Player(app, new Coordinate2D(getWidth() / 2, getHeight() / 8 * 7)));
+        addEntity(new Player(app, new Coordinate2D(getWidth() / 2, getHeight() / 8 * 6)));
+
+        for(int i = 0; i < nrBunkers; i++) {
+            int x = 50 + (i * 200);
+            addEntity(new Bunker(new Coordinate2D(x, getHeight() / 8 * 5)));
+        }
+
     }
 }
