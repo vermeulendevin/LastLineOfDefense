78ec970609ca7675b537281d29f7690c2b920be3
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-02 14:17:43 +0200

    Added collider interface

--- src/main/java/lastlineofdefense/entities/bullet/bullet.java
+++ src/main/java/lastlineofdefense/entities/bullet/bullet.java
@@ -1,11 +1,11 @@
 package lastlineofdefense.entities.bullet;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.impl.DynamicRectangleEntity;
 
-public class bullet extends DynamicRectangleEntity {
+public class bullet extends DynamicRectangleEntity implements Collider {
     private final double speed = 10;
-
 
     public bullet(Coordinate2D initialLocation, double direction) {
         super(initialLocation);
