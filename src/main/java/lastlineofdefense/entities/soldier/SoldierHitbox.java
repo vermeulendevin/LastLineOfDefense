722dbd89fc62a86a4bc4e073eedad6ebe72f49e3
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-03 16:39:33 +0200

    Added: Soldier getting removed after hit

--- src/main/java/lastlineofdefense/entities/soldier/SoldierHitbox.java
+++ src/main/java/lastlineofdefense/entities/soldier/SoldierHitbox.java
@@ -5,6 +5,7 @@ import com.github.hanyaeger.api.entities.Collided;
 import com.github.hanyaeger.api.entities.Collider;
 import com.github.hanyaeger.api.entities.impl.RectangleEntity;
 import javafx.scene.paint.Color;
+import lastlineofdefense.entities.bullet.Bullet;
 
 import java.util.List;
 
@@ -18,12 +19,11 @@ public class SoldierHitbox extends RectangleEntity implements Collided {
 
     @Override
     public void onCollision(List<Collider> collidingObject) {
-        var bulletCollision = false;
-
-//        for(Collider collider : collidingObject) {
-//            if (collider instanceof Bullet) {
-//                bulletCollision = true;
-//            }
-//        }
+        for (Collider collider : collidingObject) {
+            if (collider instanceof Bullet) {
+                ((Bullet) collider).remove();
+                remove();
+            }
+        }
     }
 }
