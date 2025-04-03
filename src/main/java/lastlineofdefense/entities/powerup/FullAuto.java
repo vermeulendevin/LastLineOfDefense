86dc4c15c176a6fb9efb5397b5fe8d6a243ff33c
Devin Vermeulen <d.vermeulen9@student.han.nl>
2025-04-03 09:12:45 +0200

    Added: Full Auto powerup

---
+++ src/main/java/lastlineofdefense/entities/powerup/FullAuto.java
@@ -0,0 +1,15 @@
+package lastlineofdefense.entities.powerup;
+
+public class FullAuto implements IPowerUp {
+
+    @Override
+    public void activatePowerUp() {
+        System.out.println("Full Auto Power Up Activated");
+
+    }
+
+    @Override
+    public void deactivatePowerUp() {
+
+    }
+}
