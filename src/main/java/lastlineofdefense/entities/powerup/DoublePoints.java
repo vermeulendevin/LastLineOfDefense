186d6d85991c786416a5fa9ece5c2f2033014f18
Marijn Zwart <amarijnzwart@gmail.com>
2025-04-03 18:20:50 +0200

    Added: DoublePoints class not functional because there's no points system yet.

---
+++ src/main/java/lastlineofdefense/entities/powerup/DoublePoints.java
@@ -0,0 +1,15 @@
+package lastlineofdefense.entities.powerup;
+
+import lastlineofdefense.entities.player.Player;
+
+public class DoublePoints implements IPowerUp{
+    @Override
+    public void activatePowerUp(Player player) {
+
+    }
+
+    @Override
+    public void deactivatePowerUp(Player player) {
+
+    }
+}
